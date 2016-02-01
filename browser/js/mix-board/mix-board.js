2e5725157d8d2cac7e4cc2ebc36bbf15b76f5460
Matt Bryan <mpbryan90@gmail.com>
2016-02-01 12:33:00 -0500

    fused DragCtrl with MixBoardController

--- browser/js/mix-board/mix-board.js
+++ browser/js/mix-board/mix-board.js
@@ -94,6 +94,62 @@ app.controller('MixBoardController', function($scope, tracks){
                 $scope.isPlaying = !$scope.isPlaying
              }
         });
+
+        //MB:draggable things below here VVVVVVVVVVVVVVVVVVVV
+        //$scope.mix is what we ng-repeat over for the playlist
+        $scope.mix = [];
+        $scope.addToMix = function (index, song, evt) {
+            var copyOfSong;
+            //MB:This is NOT to check for multiple of the same song on mix; it is to check if the song is coming from mix or library
+            if(song.onMix === false){
+                copyOfSong = new $scope.Clone(song);
+                copyOfSong.pseudoId = $scope.randomNumber();
+                $scope.mix.push(copyOfSong);
+                copyOfSong.onMix = true;
+            }
+            //MB:index is the index of the position where the draggable was dropped
+            var originalArray = $scope.mix.slice(0);
+            var originIndex;
+
+            //MB:whether it came from the playlist or the library, the draggable is put at the index position it was dropped onto
+            if (copyOfSong){
+                originIndex = $scope.mix.indexOf(copyOfSong);
+                $scope.mix[index] = copyOfSong;
+            }
+            else{
+                originIndex = $scope.mix.indexOf(song);
+                $scope.mix[index] = song;
+            }
+            //MB:shift everything based on the new position of song
+            if(index < originIndex){
+                for (var idx = originIndex; idx > index; idx--){
+                    $scope.mix[idx] = originalArray[idx - 1];
+                }
+            }
+            else{
+                for (var idx = originIndex; idx < index; idx++){
+                    $scope.mix[idx] = originalArray[idx + 1];
+                }
+            }
+        };
+        $scope.addToLibrary = function (index, song, evt) {
+            //MB: index is the index of the position where the draggable was dropped
+            var originalArray = $scope.library.slice(0);
+            var originIndex = $scope.library.indexOf(song);
+            //MB:the draggable is put at the index position it was dropped onto
+            $scope.library[index] = song;
+            //MB:start at the position the draggable came from and shift everything forward/backward
+            if(index < originIndex){
+                for (var idx = originIndex; idx > index; idx--){
+                    $scope.library[idx] = originalArray[idx - 1];
+                }
+            }
+            else{
+                for (var idx = originIndex; idx < index; idx++){
+                    $scope.library[idx] = originalArray[idx + 1];
+                }
+            }
+        }
 })
 
 
