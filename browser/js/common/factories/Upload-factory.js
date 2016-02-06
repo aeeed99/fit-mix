7fec865f3f21084ceaca29f9de3b3da566866d1f
Nick <npalenchar@gmail.com>
2016-02-05 21:01:17 -0500

    checkpoint

--- browser/js/common/factories/Upload-factory.js
+++ browser/js/common/factories/Upload-factory.js
@@ -1,1 +1,20 @@
-app.factory()
+app.factory('UploadFactory', function(){
+    return {
+        data: null,
+        add: function() {
+            var files = document.getElementById('file').files;
+            for (var k in files) {
+                if (Number.isNaN(parseInt(k))) continue;
+                (function (file) {
+                    var fileReader = new FileReader();
+                    fileReader.onload = function (e) {
+                        // get file content
+                        var text = e.target.result;
+                        console.log("result:", text);
+                    };
+                    fileReader.readAsArrayBuffer(file);
+                })(files[k]);
+            }
+        }
+    }
+});
