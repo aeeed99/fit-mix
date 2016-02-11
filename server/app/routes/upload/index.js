fb694d2413b82f1e0f7f5ae77cd3e75882dc5b62
emilyches <emilychesler@gmail.com>
2016-02-11 17:56:37 -0500

    saving multer work

--- server/app/routes/upload/index.js
+++ server/app/routes/upload/index.js
@@ -5,6 +5,10 @@ var path = require('path');
 var formidable = require('formidable'); //formidable is a node.js module for parsing form data, especially file uploads.
 var util = require('util');
 var fs = require('fs-extra');
+var multer = require('multer');
+var uploading = multer({
+  dest: __dirname + '../../../../uploads/',
+})
 
 const mongoose = require('mongoose');
 //const connectToDb = require('../server/db');
@@ -15,7 +19,6 @@ const helper = require('../../../../bin/helper');
 const metadata = require('../../../../bin/metaDataWrapper');
 Promise.promisifyAll(fs);
 
-module.exports = router;
 // save the name of the file in image field
 // then set the static path to directory with updated files
 // (so it can be used as src)
@@ -25,36 +28,9 @@ module.exports = router;
 // });
 
 
-router.post('/', function (req, res) {
-    console.log("uploading", req.body)
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('received upload:\n\n');
-        res.end(util.inspect({
-            fields: fields,
-            files: files
-        }));
-
-        new Track({
-            name: fields.title
-        }).save(function (e, result) {
-            console.log('new track saved', result);
-        });
-    });
-
-    form.on('end', function (fields, files) {
-        var temp_path = this.openedFiles[0].path;
-        var file_name = this.openedFiles[0].name;
-        var new_location = 'uploads/'; //TBD
-        fs.copy(temp_path, new_location + file_name, function (err) {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("success!");
-            }
-        });
-    });
+router.post('/', uploading.single('track'), function (req, res, next) {
+console.log("tracks", req.body)
 });
+
+
+module.exports = router;
