5d704bf2c0ca8c8f6cab4328df7401c8fc35eadb
Dan Chen <danielchen@Daniels-MacBook-Air.local>
2016-02-12 18:54:18 -0500

    tried to implement Likes

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -3,11 +3,26 @@ app.config(function($stateProvider) {
     $stateProvider.state('about', {
         url: '/user/1',
         controller: 'AboutController',
-        templateUrl: 'js/about/about.html'
+        templateUrl: 'js/about/about.html',
+        resolve: {
+            like: function(DataFactory, $stateParams) {
+                return DataFactory.fetchUser($stateParams.userId);
+            }
+        }
     });
 
 });
 
-app.controller('AboutController', function($scope, FullstackPics) {
-    // Images of beautiful Fullstack people.
+app.controller('AboutController', function($scope, like, ListFactory) {
+    $scope.like = like;
+    $scope.message;
+    $scope.addToList = function(like) {
+        ListFactory.addToList(likeId)
+        .then(function(list) {
+            $scope.list = list;
+        })
+        .then(null, function(err) {
+            $scope.message = err.message;
+        });
+    };
 });
